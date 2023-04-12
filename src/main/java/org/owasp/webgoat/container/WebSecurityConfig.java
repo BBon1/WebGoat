354684b26dad1c89d76435da143e6b9f6056c280
Boni <100412444+BBon1@users.noreply.github.com>
2023-04-12 13:35:51 +0200

    Update WebSecurityConfig.java

--- src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
+++ src/main/java/org/owasp/webgoat/container/WebSecurityConfig.java
@@ -86,7 +86,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Autowired
   public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-      auth.userDetailsService(null).passwordEncoder(new BCryptPasswordEncoder());
+      auth.userDetailsService(userDetailsService).passwordEncoder(new BCryptPasswordEncoder());
   }
 
   @Bean
