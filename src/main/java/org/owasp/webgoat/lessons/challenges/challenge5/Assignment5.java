fc70e7dd48a454e0e47005a627e49603d1f39819
Trower23 <o.lozano.2020@alumnos.urjc.es>
2023-04-12 02:23:42 +0200

    Solved static query vulnerability

--- src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
+++ src/main/java/org/owasp/webgoat/lessons/challenges/challenge5/Assignment5.java
@@ -55,7 +55,7 @@ public class Assignment5 extends AssignmentEndpoint {
       return failed(this).feedback("user.not.larry").feedbackArgs(username_login).build();
     }
     try (var connection = dataSource.getConnection()) {
-      String query = "select password from challenge_users where userid = ? and password = ?";
+      String query = "select password from challenge_users where userid = '?' and password = '?'";
       /*PreparedStatement statement =
           connection.prepareStatement(
               "select password from challenge_users where userid = '"
